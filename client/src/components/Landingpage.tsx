705aa9f99163939e58b9225341a43aba396575ed
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-04-01 15:35:45 +0530

    typo

--- client/src/components/Landingpage.tsx
+++ client/src/components/Landingpage.tsx
@@ -12,7 +12,7 @@ function Copyright() {
     <Typography variant="body2" color="text.secondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="/">
-        ezDelivery
+        ezDeliver
       </Link>{" "}
       {new Date().getFullYear()}
     </Typography>
