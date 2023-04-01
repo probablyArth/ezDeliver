66a2708215009f8b98f692cdbcaa1a491ea9feb1
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-04-01 14:21:33 +0530

    created signup page

---
+++ client/src/components/Dashboard/PreviousOrders.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  from: string,
+  to: string,
+  vehicle: string,
+  items: string,
+  orderDate: string,
+  orderStatus: string,
+) {
+  return { from, to, vehicle, items, orderDate, orderStatus };
+}
+
+const rows = [
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+  createData( 'Delhi', 'Banglore', 'Small', 'Item 23', '12:23:AM, 01-04-2023', 'Booked'),
+];
+
+export default function PreviousOrders() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>From Address</TableCell>
+            <TableCell align="right">To Address</TableCell>
+            <TableCell align="right">Vehicle</TableCell>
+            <TableCell align="right">Item</TableCell>
+            <TableCell align="right">Order Date</TableCell>
+            <TableCell align="right">Order Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.from}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.from}
+              </TableCell>
+              <TableCell align="right">{row.to}</TableCell>
+              <TableCell align="right">{row.vehicle}</TableCell>
+              <TableCell align="right">{row.items}</TableCell>
+              <TableCell align="right">{row.orderDate}</TableCell>
+              <TableCell align="right">{row.orderStatus}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- server/index.ts
+++ server/index.ts
@@ -6,9 +6,101 @@ import { config } from "dotenv";
 import morgan from "morgan";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 config({ path: "./.env" });
+
+const seedVehicles = async () => {
+  const vehicles = await prisma.vehicle.count({
+    where: {
+      OR: [
+        {
+          name: "small",
+        },
+        {
+          name: "semi",
+        },
+        {
+          name: "medium",
+        },
+        {
+          name: "heavy",
+        },
+      ],
+    },
+  });
+  if (vehicles === 0) {
+    prisma.vehicle
+      .createMany({
+        data: [
+          {
+            name: "small",
+            pricePerKm: 50,
+            weightLimit: 50,
+          },
+          {
+            name: "semi",
+            pricePerKm: 100,
+            weightLimit: 100,
+          },
+          {
+            name: "medium",
+            pricePerKm: 200,
+            weightLimit: 200,
+          },
+          {
+            name: "heavy",
+            pricePerKm: 400,
+            weightLimit: 500,
+          },
+        ],
+      })
+      .then(() => {
+        console.log("Added vehicles.");
+      });
+  }
+};
+
+const seedOnce = async () => {
+  const user = await prisma.user.create({
+    data: {
+      phone: "+919292929292",
+      username: "ramlal",
+    },
+  });
+  const smallVehicleId = await prisma.vehicle.findFirst({
+    where: {
+      name: "small",
+    },
+    select: {
+      id: true,
+    },
+  });
+  await prisma.delievery.create({
+    data: {
+      sellerId: user.id,
+      bookingDate: new Date(),
+      from: "191919",
+      to: "202020",
+      items: {
+        createMany: {
+          data: [
+            {
+              name: "Potato",
+              weight: 10,
+            },
+            {
+              name: "Mooli",
+              weight: 20,
+            },
+          ],
+        },
+      },
+      vehicleId: smallVehicleId?.id,
+    },
+  });
+  console.log("User and delievery seed created");
+};
 
 const app = express();
 const PORT = (process.env.PORT && parseInt(process.env.PORT)) || 4000;
@@ -22,6 +114,8 @@ app.get("/", async (req, res) => {
   return res.json({ message: "hi" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server listening at ${PORT}`);
+  await seedVehicles();
+  // await seedOnce();
 });
