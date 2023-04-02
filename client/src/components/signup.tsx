2beebc535eeebe3402fdf9abd4b3f3bae474281a
amanscisingh <aman.singh.civ19@itbhu.ac.in>
2023-04-02 09:52:42 +0530

    bug fix

--- client/src/components/signup.tsx
+++ client/src/components/signup.tsx
@@ -182,7 +182,6 @@ export default function Signup() {
               <Grid container justifyContent="flex-end">
                 <Grid item>
                   <Link href="/login" variant="body2">
-                  <Link href="/login" variant="body2">
                     Already have an account? Log in
                   </Link>
                 </Grid>
